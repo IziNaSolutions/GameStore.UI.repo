228c44b46b60539e887705d2cfe0daf08d1e6f4d
eliran nachmani <elirannach@gmail.com>
2017-08-01 17:43:25 +0300

    eliran's cart update

--- app/providers/session.js
+++ app/providers/session.js
@@ -14,138 +14,138 @@
  * @param $rootScope
  */
 angular.module('gameStoreApp')
-  .service('session', function ($cookies, $rootScope, $log) {
+    .service('session', function($cookies, $rootScope, $log) {
 
-    ///////////// Initiate ///////////
+        ///////////// Initiate ///////////
 
-    if ($rootScope.user === undefined) {
-      $rootScope.user = {
-        isConnected: false,
-        role: 'guest',
-        userName: 'Guest',
-      };
-    }
-
-    var _cookieKey = 'GameStoreUser';
-
-    ///////////// Private Methods //////////
-
-
-    //////////// Public Methods ////////////
-
-    // METHODS
-
-    /**
-     * @public
-     * @desc
-     * 
-     *    TODO - Amit
-     * 
-     * @param {user} user 
-     *     "userName": {string},
-     *     "password": {string},
-     *     "firstName": {string},
-     *     "lastName": {string},
-     *     "country": {string - from XML file},
-     *     "address": {string},
-     *     "phone": {string},
-     *     "ansFirstQ": {string},
-     *     "ansSecondQ": {string},
-     *     "categories": {string[]}
-     */
-    function connect(user) {
-      $rootScope.user = user;
-      $log.info('user: ' + user.userName + ' session is logged in', user);
-    }
-
-    /**
-     * @public
-     * @desc
-     *      use the $cookies provider to store the user object
-     *      as a cookie at the client computer.
-     * 
-     * @return {string} cookie key name
-     * 
-     * @param {user} user 
-     */
-    function remember(user) {
-
-      var currentDate = new Date(Date.now());
-      $log.info('current Date:', currentDate);
-
-      var expiration = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDay() + 1);
-
-      $cookies.putObject(
-        _cookieKey,
-        user, {
-          expires: expiration,
+        if ($rootScope.user === undefined) {
+            $rootScope.user = {
+                isConnected: false,
+                role: 'guest',
+                userName: 'Guest',
+            };
         }
-      );
 
-      $log.info('session.remember user:', user);
-      $log.info('session.remember expiration date:', expiration);
-      $log.info('session.remember cookie key:', _cookieKey);
-    }
+        var _cookieKey = 'GameStoreUser';
 
-    /**
-     * @public
-     * @desc
-     * 
-     *  TODO - Amit
-     *  
-     * @return {user} current session
-     */
-    function get() {
-      var storedUser = null;
-
-      storedUser = $cookies.getObject(_cookieKey);
-      $log.debug('stored cookie:', storedUser);
-
-      if (!$rootScope.isConnected && storedUser !== null && storedUser !== undefined) {
-        $rootScope.user = storedUser;
-        $rootScope.user.isConnected = true;
-        $log.debug('is session.get() -> took cookie');
-      }
-
-      $log.info('in session.get()', $rootScope.user);
-
-      return $rootScope.user;
-    }
-
-    /**
-     * @public
-     * 
-     * @desc
-     *      logging the user out - delete cookie
-     */
-    function logout() {
-
-      $rootScope.user = {
-        isConnected: false,
-        role: 'guest',
-        userName: 'Guest',
-      };
-
-      // TODO - amit to test - see if exception not thrown here if cookie not exists...
-      $cookies.remove(_cookieKey);
-
-    }
+        ///////////// Private Methods //////////
 
 
-    // API
-    var API = {
-      get: get,
-      remember: remember,
-      connect: connect,
-      logout: logout,
-    };
+        //////////// Public Methods ////////////
 
-    //////// root scope methods publish //////
+        // METHODS
 
-    $rootScope.logout = logout;
-    $rootScope.get = get;
+        /**
+         * @public
+         * @desc
+         * 
+         *    TODO - Amit
+         * 
+         * @param {user} user 
+         *     "userName": {string},
+         *     "password": {string},
+         *     "firstName": {string},
+         *     "lastName": {string},
+         *     "country": {string - from XML file},
+         *     "address": {string},
+         *     "phone": {string},
+         *     "ansFirstQ": {string},
+         *     "ansSecondQ": {string},
+         *     "categories": {string[]}
+         */
+        function connect(user) {
+            $rootScope.user = user;
+            $log.info('user: ' + user.userName + ' session is logged in', user);
+        }
 
-    ////////////// DONE ////////////////
-    return API;
+        /**
+         * @public
+         * @desc
+         *      use the $cookies provider to store the user object
+         *      as a cookie at the client computer.
+         * 
+         * @return {string} cookie key name
+         * 
+         * @param {user} user 
+         */
+        function remember(user) {
 
-  });
+            var currentDate = new Date(Date.now());
+            $log.info('current Date:', currentDate);
+
+            var expiration = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDay() + 1);
+
+            $cookies.putObject(
+                _cookieKey,
+                user, {
+                    expires: expiration,
+                }
+            );
+
+            $log.info('session.remember user:', user);
+            $log.info('session.remember expiration date:', expiration);
+            $log.info('session.remember cookie key:', _cookieKey);
+        }
+
+        /**
+         * @public
+         * @desc
+         * 
+         *  gets the current user object
+         *  
+         * @return {user} current session
+         */
+        function get() {
+            var storedUser = null;
+
+            storedUser = $cookies.getObject(_cookieKey);
+            $log.debug('stored cookie:', storedUser);
+
+            if (!$rootScope.isConnected && storedUser !== null && storedUser !== undefined) {
+                $rootScope.user = storedUser;
+                $rootScope.user.isConnected = true;
+                $log.debug('is session.get() -> took cookie');
+            }
+
+            $log.info('in session.get()', $rootScope.user);
+
+            return $rootScope.user;
+        }
+
+        /**
+         * @public
+         * 
+         * @desc
+         *      logging the user out - delete cookie
+         */
+        function logout() {
+
+            $rootScope.user = {
+                isConnected: false,
+                role: 'guest',
+                userName: 'Guest',
+            };
+
+            // TODO - amit to test - see if exception not thrown here if cookie not exists...
+            $cookies.remove(_cookieKey);
+
+        }
+
+
+        // API
+        var API = {
+            get: get,
+            remember: remember,
+            connect: connect,
+            logout: logout,
+        };
+
+        //////// root scope methods publish //////
+
+        $rootScope.logout = logout;
+        $rootScope.get = get;
+
+        ////////////// DONE ////////////////
+        return API;
+
+    });
