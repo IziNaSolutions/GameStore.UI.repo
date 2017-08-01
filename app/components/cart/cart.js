708a87a5103756332aa9e456370d0d844a8ad0fc
eliran nachmani <elirannach@gmail.com>
2017-08-01 19:57:49 +0300

    eliran's cart update

--- app/components/cart/cart.js
+++ app/components/cart/cart.js
@@ -10,45 +10,40 @@
 angular.module('gameStoreApp')
     .controller('CartCtrl', function(cart, session, $log, commonHttp) {
 
-            /////// init /////////
+        /////// init /////////
 
-            var cartSelf = this;
+        var cartSelf = this;
 
-            cartSelf.user = session.get();
+        cartSelf.user = session.get();
 
-            cartSelf.baseUrl = commonHttp.GetServiceBaseURL();
+        cartSelf.baseUrl = commonHttp.GetServiceBaseURL();
 
-            // cartSelf.cartProducts = [{
-            //         gameName: 'fifa17',
-            //         price: 10,
-            //         amountInCart: 2,
-            //         image: '/images/' + 'fifa17.jpg',
-            //     },
-            //     {
-            //         gameName: 'fifa17',
-            //         price: 10,
-            //         amountInCart: 2,
-            //         image: '/images/' + 'fifa17.jpg',
-            //     },
-            // ];
+        // cartSelf.cartProducts = [{
+        //         gameName: 'fifa17',
+        //         price: 10,
+        //         amountInCart: 2,
+        //         image: '/images/' + 'fifa17.jpg',
+        //     },
+        //     {
+        //         gameName: 'fifa17',
+        //         price: 10,
+        //         amountInCart: 2,
+        //         image: '/images/' + 'fifa17.jpg',
+        //     },
+        // ];
 
-            cartSelf.total = 0;
+        cartSelf.total = 0;
 
-            cart.getCartInfo(cartSelf.user.userName)
-                .then(function(res) {
-                    $log.info("getCartInfo response:", res);
-                    cartSelf.cartProducts = res;
+        cart.getCartInfo(cartSelf.user.userName)
+            .then(function(res) {
+                $log.info("getCartInfo response:", res);
+                cartSelf.cartProducts = res;
 
-                    for (var i = 0; i < cartSelf.cartProducts.length; i++) {
-                        cartSelf.total += cartSelf.cartProducts[i].price * cartSelf.cartProducts[i].amountInCart;
-                    }
+                for (var i = 0; i < cartSelf.cartProducts.length; i++) {
+                    cartSelf.total += cartSelf.cartProducts[i].price * cartSelf.cartProducts[i].amountInCart;
+                }
 
-                });
-            cartSelf.refresh = function() {
-                cartSelf.getCartInfo();
-            };
-
-        }
+            });
 
 
     });
